1325915868efac4a1e2962296176a83925d1cdc5
Sylvain <164896873+SylvainBertaigne@users.noreply.github.com>
2024-04-28 16:36:28 +0200

    Reprise de la mise en forme
    Modification complète du CSS.
    Retour à zéro.

---
+++ test/app.js
@@ -0,0 +1,7 @@
+const button = document.querySelector("button");
+
+button.onclick = function () {
+  let name = prompt("Quel est votre nom;?");
+  alert("Salut " + name + ", sympa de vous voir;!");
+};
+
